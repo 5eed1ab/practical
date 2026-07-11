72754504d6afaaf8d348931790b9ce3580dd4839
j0hnniewa1ker <jonwalk@icloud.com>
2026-07-11 11:51:11 -0500

    Function declarations and (void *)&

--- practical.c
+++ practical.c
@@ -9,21 +9,18 @@ const char asciiCa7[] =
 "▒░░░░░░░  ▒█░  ░░░███░▒▒▒▒░░  ░▒▒░▒▒▒░░░░░▒▒▓▓▒░ ░░█▓▓▒▒▒███░░░░  █▒      ░░▒▒▒▒\n"
 "▓░▒▒░░░░ ░ ░▓░  ░░███░░░░░█░  ░░░▒░░░░░░░▒▒▒▓▓▒ ░░▒██▒░▒░███░░░░ ▓▒    ░ ░░▒▓▓▒▒\n"
 "▒░░▒░░░     ░▒▓  ░ █░░░ ░██▓░░░▒▒░ ░   ░░░▒░▒▒▒░░░▓██▓░░▒░█ ░░ ▓░   ░░░░ ░░▒▓▓▒▒\n"
-"▒░░▒▒▒░░░░      ▒▓▒▒▓▓▓▓▓▒▒▒░░▒░░░░░░░░░░▒▒▒▓▓▒▒▒▒▓▓▒▓▒▓▓▓▓▓░░     ░░░░░▒░▒▓▓▓▓▒\n"
-"";
+"▒░░▒▒▒░░░░      ▒▓▒▒▓▓▓▓▓▒▒▒░░▒░░░░░░░░░░▒▒▒▓▓▒▒▒▒▓▓▒▓▒▓▓▓▓▓░░     ░░░░░▒░▒▓▓▓▓▒\n";
 
-void printCa7(int episode) {
-    printf("Practical Ca7");
-    printf("%67b\n", episode);
-    printf("%s",asciiCa7);
-}
+// Function declarations
+void printCa7(int episode);
+int BitShiftMask(char c);
 
 int main(){
   int i =0;
-  char character = 65;
-  printCa7(character);
   float pi = 4 * atan(1.0);
+  char character = 'A';
   char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  printCa7(character);
   printf("%s",alphabet);
   printf(": printf %%s alphabet[] = \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\";\n");
   while(alphabet[i] != '\0'){
@@ -32,20 +29,22 @@ int main(){
   }
   printf(": loop through alphabet[i] until \'\\0\' null terminator\n\n");
   printf("for loop adding 1 each pass 26 times\n");
-  char lowercase = character | (1 << 5);
-    /* Bit Shift Mask:
-      - creates a binary number where only the 6th bit is set
-      - The | operator ensures that the 6th bit is always set 
-      (effectively converting uppercase to lowercase)
-                   *      
-                 76543210  
-      'A' → 65 → 01000001
-      'a' → 97 → 01100001
-                   */
+
   for(i=0;i<26;i++){
-    printf("%c → %d → %b |  %c → %3d → %b\n",
-      character+i,character+i,character+i,
-      lowercase+i,lowercase+i,lowercase+i
+    char lowercase = BitShiftMask(alphabet[i]);
+    printf("%p → %c → %d → %b | %p → %c → %3d → %b\n",
+      (void *)&alphabet[i],alphabet[i],alphabet[i],alphabet[i],
+      (void *)&lowercase,lowercase,lowercase,lowercase
+
+    );
+  }
+  printf("\n");
+  int c;
+  for(c=65;c<91;c++){
+    char lowercase = BitShiftMask(c);
+    printf("%p → %c → %d → %b | %p → %c → %3d → %b\n",
+      (void *)&c,c,c,c,
+      (void *)&lowercase,lowercase,lowercase,lowercase
     );
   }
   printf("\n");
@@ -67,4 +66,27 @@ int main(){
   }
   printf("\n");
   return 0;
+}
+
+// Functions
+void printCa7(int episode) 
+{
+    printf("Practical Ca7");
+    printf("%67b\n", episode);
+    printf("%s",asciiCa7);
+}
+
+int BitShiftMask(char c)
+{
+  char lowercase = c | (1 << 5);
+  /* Bit Shift Mask:
+    - creates a binary number where only the 6th bit is set
+    - The | operator ensures that the 6th bit is always set 
+    (effectively converting uppercase to lowercase)
+                  *      
+                76543210  
+    'A' → 65 → 01000001
+    'a' → 97 → 01100001
+                  */
+  return lowercase;
 }
